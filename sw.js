caf5004ffd865c1aae6a775e007ccf69910c9bfd
marcmylemans <marcmylemans@gmail.com>
2024-01-07 16:30:20 +0100

    m

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704640552';
+const cacheName = 'chirpy-1704641357';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
