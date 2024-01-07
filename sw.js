21799e733d2c766742b8592de8f7c03d326e8a15
marcmylemans <marcmylemans@gmail.com>
2024-01-07 14:31:10 +0100

    m

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704634061';
+const cacheName = 'chirpy-1704634198';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
